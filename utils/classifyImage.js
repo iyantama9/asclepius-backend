2026cbd706dbeecc857cfe1abd2027e8c24d1108
iyantama9 <pratamawhilyan399@gmail.com>
2024-11-30 02:55:13 +0700

    add

---
+++ utils/classifyImage.js
@@ -0,0 +1,27 @@
+const tf = require('@tensorflow/tfjs-node');
+
+async function classifyImage(model, imageData) {
+    try {
+        const tensor = tf.node
+            .decodeJpeg(imageData)
+            .resizeNearestNeighbor([224, 224])
+            .expandDims()
+            .toFloat();
+
+        const prediction = model.predict(tensor);
+        const scores = await prediction.data();
+        const confidenceScore = Math.max(...scores) * 100;
+
+        const classificationLabel = confidenceScore > 50 ? 'Cancer' : 'Non-cancer';
+        const recommendation =
+            classificationLabel === 'Cancer'
+                ? 'Segera periksa ke dokter!'
+                : 'Penyakit kanker tidak terdeteksi.';
+
+        return { classificationLabel, recommendation };
+    } catch (error) {
+        throw new ValidationError('Terjadi kesalahan dalam melakukan prediksi');
+    }
+}
+
+module.exports = classifyImage;
